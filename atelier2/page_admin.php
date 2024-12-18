0953e33c64d7f269682d5e8e91b0d007ca6c62ae
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-18 21:23:05 +0100

    Update page_admin.php

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -4,10 +4,12 @@ session_start();
 
 // Vérifier si l'utilisateur est bien en possession d'un cookie valide
 // Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (isset($_COOKIE['authToken']) && !str_starts_with($_COOKIE['authToken'], 'admin_')) {
-    header('Location: index.php');
+if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'admin_')) {
+    header('Location: page_admin.php');
     exit();
-}
+
+} elseif (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user_')) {
+    header('location:user.php
 ?>
 
 <!DOCTYPE html>

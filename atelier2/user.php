097aaf9f8ca45d45cf1576faa285dbb7eb32f12a
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-18 21:40:16 +0100

    Update user.php

--- atelier2/user.php
+++ atelier2/user.php
@@ -22,25 +22,8 @@ if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user
 </head>
 <body>
     <h1>Bienvenue sur la page Administrateur protégée par un Cookie</h1>
-    <p>Vous êtes connecté en tant qu'admin.</p>
-    <a href="logout.php">Se déconnecter</a>
-</body>
-</html>_')) {
-    header('Location: index.php');
-    exit();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
-</head>
-<body>
-    <h1>Bienvenue sur la page Administrateur protégée par un Cookie</h1>
-    <p>Vous êtes connecté en tant qu'admin.</p>
+    <p>Vous êtes connecté en tant qu'un utilisateur.</p>
     <a href="logout.php">Se déconnecter</a>
 </body>
 </html>
+

8eb4416080449a400ef4327c4f7c79491e4c94de
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-20 11:26:13 +0100

    Update page_user.php

--- atelier3/page_user.php
+++ atelier3/page_user.php
@@ -4,7 +4,7 @@
 session_start();
 
 // Vérifier si l'utilisateur s'est bienconnecté
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']=== 'user') {
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']=== true && $_SESSION['username']!== 'user') {
     header('Location: index.php'); // Dans le cas contraire, l'utilisateur sera redirigé vers la page de connexion
     exit();
 }

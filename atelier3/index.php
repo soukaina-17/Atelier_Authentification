58c53d6684bb8db61fb6bcb5d0e3728b01d97173
soukaina-17 <soukaina.ljaouhari@gmail.com>
2024-12-20 10:46:22 +0100

    Update index.php

--- atelier3/index.php
+++ atelier3/index.php
@@ -5,7 +5,7 @@ session_start();
 // Vérifier si l'utilisateur est déjà connecté
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     header('Location: page_admin.php'); // Si l'utilisateur s'est déjà connecté alors il sera automatiquement redirigé vers la page protected.php
-    } else {
+    } elseif ($_SESSION['username'] === 'user') {
         header('Location: page_user.php'); // Page pour utilisateur
     }
     exit();
